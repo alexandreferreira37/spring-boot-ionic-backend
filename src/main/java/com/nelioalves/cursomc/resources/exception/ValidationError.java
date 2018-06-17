dcb8f17468d09040ef03e9c27d518ceb76daf412
Alexandre Ferreira <almcfx@gmail.com>
2018-06-17 18:55:23 -0300

    PUT, DELETE E GET para cliente

--- src/main/java/com/nelioalves/cursomc/resources/exception/ValidationError.java
+++ src/main/java/com/nelioalves/cursomc/resources/exception/ValidationError.java
@@ -11,7 +11,7 @@ public class ValidationError extends StandardError{
 	
 	public ValidationError(Integer status, String msg, Long timeStamp) {
 		super(status, msg, timeStamp);
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public List<FieldMessage> getErrors() {
