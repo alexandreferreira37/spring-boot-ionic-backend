d46e6622fe8c8a9e7cddfb1cfd1852211a1150c7
Alexandre Ferreira <almcfx@gmail.com>
2019-01-27 22:08:48 -0200

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/MockEmailService.java
@@ -1,5 +1,7 @@
 package com.nelioalves.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
@@ -9,12 +11,19 @@ public class MockEmailService extends AbstractEmailService {
 	private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 	
 	@Override
-	public void sendEmail(SimpleMailMessage msg) {
-		// TODO Auto-generated method stub
-		
+	public void sendEmail(SimpleMailMessage msg) {			
 		LOG.info("Simulando envio de email...");
 		LOG.info(msg.toString());
 		LOG.info("Email enviado.");
 	}
+
+	@Override
+	public void sendHtmlEmail(MimeMessage msg) {
+		LOG.info("Simulando envio de email HTML...");
+		LOG.info(msg.toString());
+		LOG.info("Email enviado.");		
+	}
+	
+	
 
 }
